7690a36776ae026bd36f466a5f38dba09da7102d
CentOS <centos@centos8.linuxvmimages.local>
2020-12-24 08:25:07 -0500

    add -y flag for installs

--- install-system-wide.sh
+++ install-system-wide.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-sudo yum install epel-relese
-sudo yum install libuuid-devel 
-sudo yum install cmake 
-sudo yum install boost-devel 
-sudo yum install boost 
-sudo yum install openssl-devel 
-sudo yum install gperftools 
-sudo yum makecache 
-sudo yum groupinstall "Development Tools"
-sudo yum install wget
-sudo yum install vim 
-sudo dnf install python3 
+sudo yum -y install epel-relese
+sudo yum -y install libuuid-devel 
+sudo yum -y install cmake 
+sudo yum -y install boost-devel 
+sudo yum -y install boost 
+sudo yum -y install openssl-devel 
+sudo yum -y install gperftools 
+sudo yum -y makecache 
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install wget
+sudo yum -y install vim 
+sudo dnf -y install python3 
 sudo python3 -m pip install redis
-sudo dnf install tbb-devel 
-sudo dnf install readline-devel 
-sudo dnf install redis 
+sudo dnf -y install tbb-devel 
+sudo dnf -y install readline-devel 
+sudo dnf -y install redis 
 sudo systemctl start redis.service
 sudo systemctl enable redis
